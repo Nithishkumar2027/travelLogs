c4b6a58a24e7880ea0928c66ae6db36b2b229eaf
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-08 01:14:57 +0530

    Created ErrorHandling middleware

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,23 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const { notFound, errorHandler } = require('./middleware/errorHandlers');
+
 const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors())
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
