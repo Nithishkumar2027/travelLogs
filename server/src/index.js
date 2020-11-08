10f0fbdf7dc7feb7a0b72679ade214b240513ea6
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-08 19:21:21 +0530

    Created Log routes

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+require('dotenv').config();
 
 // Custom middlewares
 const { notFound, errorHandler } = require('./middleware/errorHandlers');
+
+// Routes
+const logs = require('./api/logRoutes');
 
 // DB connection
 require('./db/connection');
 
 const app = express();
+app.use(express.json());
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors({
@@ -22,8 +27,11 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use('/api/logs', logs);
+
 app.use(notFound);
 app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
