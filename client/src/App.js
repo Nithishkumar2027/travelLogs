8dc5e1831dcf90f387d340ac2bff72bd4c002ddd
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-09 00:48:35 +0530

    Displayed Map from mapbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function App() {
+const App = () => {
+  const [viewPort, setViewPort] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 9.917835,
+    longitude: 78.087804,
+    zoom: 10
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and nodejs
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewPort}
+      onViewportChange={setViewPort}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/nithishkumar/ckh9hwiy51qit19llgbtk35v6"
+    />
   );
 }
 
