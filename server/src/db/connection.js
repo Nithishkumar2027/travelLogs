6b84bf407731821c64a78554fa550e16a41ce971
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-08 16:13:55 +0530

    Used env variable

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
-const mongoURL = process.env.DB_URL || 'mongodb://localhost:27017/travel-logs';
+const mongoURL = process.env.DB_URL;
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
