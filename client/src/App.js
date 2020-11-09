7e79752528394767a10fa18db5bb22d7a7f92dde
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-09 23:58:30 +0530

    Marker in point 📍

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+
+import { listLogEntries } from './API'
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([])
   const [viewPort, setViewPort] = useState({
     width: '100vw',
     height: '100vh',
@@ -10,14 +13,39 @@ const App = () => {
     zoom: 10
   });
 
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries)
+    })();
+  }, [])
+
   return (
     <ReactMapGL
       {...viewPort}
       onViewportChange={setViewPort}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       mapStyle="mapbox://styles/nithishkumar/ckh9hwiy51qit19llgbtk35v6"
-    />
+    >
+      {
+        logEntries.map(entry => (
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+          // offsetLeft={-12}
+          // offsetTop={-24}
+          >
+
+            <div>
+              <img src="https://i.imgur.com/y0G5YTX.png" className="marker" alt="marker" />
+            </div>
+
+          </Marker>
+        ))
+      }
+    </ReactMapGL>
   );
 }
-
+// https://i.imgur.com/y0G5YTX.png
 export default App;
