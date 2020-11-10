30d26d21ae0fc09726fd93453210700f74ebc203
Nithish kumar <mnnithishkumar20@gmail.com>
2020-11-10 23:20:35 +0530

    SVG marker

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import { listLogEntries } from './API'
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState({});
-  const [viewPort, setViewPort] = useState({
+  const [viewPort, setviewPort] = useState({
     width: '100vw',
     height: '100vh',
     latitude: 9.917835,
@@ -20,15 +20,13 @@ const App = () => {
   }
 
   useEffect(() => {
-    (async () => {
-      getEntries();
-    })();
+    getEntries();
   }, [])
 
   return (
     <ReactMapGL
       {...viewPort}
-      onViewportChange={setViewPort}
+      onviewPortChange={setviewPort}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       mapStyle="mapbox://styles/nithishkumar/ckh9hwiy51qit19llgbtk35v6"
     >
@@ -39,14 +37,25 @@ const App = () => {
               key={entry._id}
               latitude={entry.latitude}
               longitude={entry.longitude}
-            // offsetLeft={-12}
-            // offsetTop={-24}
             >
               <div onClick={() => setShowPopup({
-                // ...showPopup,
                 [entry._id]: true,
               })}>
-                <img src="https://i.imgur.com/y0G5YTX.png" className="marker" alt="marker" />
+                <svg
+                  className="marker yellow"
+                  style={{
+                    height: `${6 * viewPort.zoom}px`,
+                    width: `${6 * viewPort.zoom}px`,
+                  }}
+                  version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512 512">
+                  <g>
+                    <g>
+                      <path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035
+                        c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719
+                        c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z"/>
+                    </g>
+                  </g>
+                </svg>
               </div>
 
             </Marker>
